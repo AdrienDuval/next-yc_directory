3ed2db8e573fdb80148ac3fa0689904a3a2aa08f
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2025-01-15 20:10:36 +0530

    Working on the hero component and the search form
    Need to continue here 01:55:30 — All Startups & Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your Startup, <br /> Connect with Entrpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and get Noticed in Virtual Competition
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
