2dcb70a40517086d16549bb12c248c1b294d1ed5
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2025-01-16 18:44:40 +0530

    Sanity and card layouts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,35 @@
 import SearchForm from "@/components/SearchForm";
+import StarupCard from "@/components/StarupCard";
 
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) {
+  interface StartupCardType {
+    _createAt: object;
+    views: number;
+    author: { _id: number };
+    _id: number;
+    description: string;
+    image: string;
+    category: string;
+    title: string;
+  }
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Adrien" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://i.pinimg.com/736x/26/99/ae/2699ae26a1ce8a2e724e0e6ad7a9cbf1.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -16,6 +40,19 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and get Noticed in Virtual Competition
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search resultes for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0
+            ? posts.map((post: StartupCardType) => (
+                <StarupCard key={post?._id} post={post} />
+              ))
+            : ""}
+        </ul>
       </section>
     </>
   );
